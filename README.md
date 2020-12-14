e95a3e2ee1670c7f40369989efdb10ffa6e1d436
Seany Computer <sean@wiredviews.com>
2020-12-13 19:37:02 -0500

    feat(sln): initial commit of projects

--- README.md
+++ README.md
@@ -1,1 +1,206 @@
-# xperience-query-extensions
+# Xperience Query Extensions
+
+[![NuGet Package](https://img.shields.io/nuget/v/WiredViews.Xperience.QueryExtensions.svg)](https://www.nuget.org/packages/WiredViews.Xperience.QueryExtensions)
+
+This package provides a set of extension methods for Kentico Xperience 13.0 `DocumentQuery`, `MultiDocumentQuery`, and `ObjectQuery` [data access APIs](https://docs.xperience.io/13api/content-management/pages).
+
+## Dependencies
+
+This package is compatible with ASP.NET Core 3.1 -> ASP.NET Core 5 applications or libraries integrated with Kentico Xperience 13.0.
+
+## How to Use?
+
+1. Install the NuGet package in your ASP.NET Core project (or class library)
+
+   ```bash
+   dotnet add package WiredViews.Xperience.QueryExtensions
+   ```
+
+1. The extension methods are all in the `CMS.DocumentEngine` and `CMS.DataEngine` namespaces, so assuming you have the package installed you should see them appear in Intellisense.
+
+## Extension Method Examples
+
+### DocumentQuery
+
+```csharp
+public void QueryDocument(Guid nodeGuid)
+{
+    var query = DocumentHelper.GetDocuments()
+        .WhereNodeGUIDEquals(nodeGuid);
+}
+```
+
+```csharp
+var query = DocumentHelper.GetDocuments()
+    .OrderByNodeOrder();
+```
+
+```csharp
+var query = DocumentHelper.GetDocuments()
+    .OrderByDescending(nameof(TreeNode.NodeID))
+    .TopN(1)
+    .DebugQuery();
+
+/*
+~~~ BEGIN [path\to\your\app\Program.cs] QUERY ~~~
+
+
+DECLARE @DocumentCulture nvarchar(max) = N'en-US';
+
+SELECT TOP 1 *
+FROM View_CMS_Tree_Joined AS V WITH (NOLOCK, NOEXPAND) LEFT OUTER JOIN COM_SKU AS S WITH (NOLOCK) ON [V].[NodeSKUID] = [S].[SKUID]
+WHERE [DocumentCulture] = @DocumentCulture
+ORDER BY NodeID DESC
+
+
+~~~ END [path\to\your\app\Program.cs] QUERY ~~~
+*/
+```
+
+```csharp
+var query = DocumentHelper.GetDocuments()
+    .OrderByDescending(nameof(TreeNode.NodeID))
+    .TopN(1)
+    .DebugQuery("Newest Document");
+
+/*
+~~~ BEGIN [Newest Document] QUERY ~~~
+
+
+DECLARE @DocumentCulture nvarchar(max) = N'en-US';
+
+SELECT TOP 1 *
+FROM View_CMS_Tree_Joined AS V WITH (NOLOCK, NOEXPAND) LEFT OUTER JOIN COM_SKU AS S WITH (NOLOCK) ON [V].[NodeSKUID] = [S].[SKUID]
+WHERE [DocumentCulture] = @DocumentCulture
+ORDER BY NodeID DESC
+
+
+~~~ END [Newest Document] QUERY ~~~
+*/
+```
+
+```csharp
+public void QueryDatabase(ILogger logger)
+{
+    var query = DocumentHelper.GetDocuments()
+        .OrderByDescending(nameof(TreeNode.NodeID))
+        .TopN(1)
+        .LogQuery(logger, "Logged Query");
+}
+```
+
+```csharp
+public async Task QueryDatabase(CancellationToken token)
+{
+    List<TreeNode> pages = await DocumentHelper.GetDocuments()
+        .TopN(5)
+        .ToListAsync(token);
+}
+```
+
+```csharp
+public async Task QueryDatabase(CancellationToken token)
+{
+    TreeNode? newestPage = await DocumentHelper.GetDocuments()
+        .OrderByDescending(nameof(TreeNode.NodeID))
+        .TopN(1)
+        .FirstOrDefaultAsync(token);
+
+    if (newestPage is null)
+    {
+        return;
+    }
+
+    // ...
+}
+```
+
+### ObjectQuery
+
+```csharp
+var query = UserInfo.Provider.Get()
+    .OrderByDescending(nameof(UserInfo.UserLastModified))
+    .TopN(1)
+    .DebugQuery();
+
+/*
+~~~ BEGIN [path\to\your\app\Program.cs] QUERY ~~~
+
+
+SELECT TOP 1 *
+FROM CMS_User
+ORDER BY UserLastModified DESC
+
+
+~~~ END [path\to\your\app\Program.cs] QUERY ~~~
+*/
+```
+
+```csharp
+var query = UserInfo.Provider.Get()
+    .OrderByDescending(nameof(UserInfo.UserLastModified))
+    .TopN(1)
+    .DebugQuery("User");
+
+/*
+~~~ QUERY [User] START ~~~
+
+
+SELECT TOP 1 *
+FROM CMS_User
+ORDER BY UserLastModified DESC
+
+
+~~~ QUERY [User] END ~~~
+*/
+```
+
+```csharp
+public void QueryDatabase(ILogger logger)
+{
+    var query = UserInfo.Provider.Get()
+        .OrderByDescending(nameof(UserInfo.UserLastModified))
+        .TopN(1)
+        .LogQuery(logger, "Logged User Query");
+}
+```
+
+```csharp
+public async Task QueryDatabase(CancellationToken token)
+{
+    List<UserInfo> recentlyUpdatedUsers = await UserInfo.Provider.Get()
+        .OrderByDesc(nameof(UserInfo.UserLastModified))
+        .TopN(10)
+        .ToListAsync(token);
+}
+```
+
+```csharp
+public async Task QueryDatabase(CancellationToken token)
+{
+    UserInfo? user = await UserInfo.Provider.Get()
+        .OrderByDesc(nameof(UserInfo.UserLastModified))
+        .TopN(1)
+        .FirstOrDefaultAsync(token);
+
+    if (user is null)
+    {
+        return;
+    }
+
+    // ...
+}
+```
+
+## References
+
+### .NET
+
+- [Nullable reference types (C# reference)](https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/nullable-reference-types)
+
+### Kentico Xperience
+
+- [Kentico Xperience 13 Beta 3 - New Data Access APIs](https://dev.to/seangwright/kentico-xperience-13-beta-3-new-data-access-apis-1oha)
+- [Pages API Examples](https://docs.xperience.io/13api/content-management/pages)
+- [Retrieving pages in custom scenarios](https://docs.xperience.io/custom-development/working-with-pages-in-the-api#WorkingwithpagesintheAPI-Retrievingpagesincustomscenarios)
+- [Improvements under the hood – Document and ObjectQuery enumeration without DataSets](https://devnet.kentico.com/articles/improvements-under-the-hood-document-and-objectquery-enumeration-without-datasets)
---
+++ Xperience.QueryExtensions/XperienceCommunityObjectQueryExtensions.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace CMS.DataEngine
+{
+    public static class XperienceCommunityObjectQueryExtensions
+    {
+        /// <summary>
+        /// Converts the <paramref name="query"/> to a <see cref="List{TObject}"/> of the generic Object type
+        /// </summary>
+        /// <param name="query">The current ObjectQuery</param>
+        /// <param name="token">Optional cancellation token</param>
+        /// <returns></returns>
+        public static async Task<IList<TObject>> ToListAsync<TObject>(this ObjectQuery<TObject> query, CancellationToken token = default)
+            where TObject : BaseInfo
+        {
+            var result = await query.GetEnumerableTypedResultAsync(cancellationToken: token);
+
+            return result.ToList();
+        }
+
+        /// <summary>
+        /// Converts the <paramref name="query"/> to a <see cref="List{BaseInfo}"/> of <see cref="BaseInfo" />
+        /// </summary>
+        /// <param name="query">The current ObjectQuery</param>
+        /// <param name="token">Optional cancellation token</param>
+        /// <returns></returns>
+        public static async Task<IList<BaseInfo>> ToListAsync(this ObjectQuery query, CancellationToken token = default)
+        {
+            var result = await query.GetEnumerableTypedResultAsync(cancellationToken: token);
+
+            return result.ToList();
+        }
+
+        /// <summary>
+        /// Returns the first item of the <paramref name="query"/> as the generic Object type and <see cref="null" /> if no items were returned.
+        /// </summary>
+        /// <param name="query">The current ObjectQuery</param>
+        /// <param name="token">Optional cancellation token</param>
+        /// <returns></returns>
+        public static async Task<TObject?> FirstOrDefaultAsync<TObject>(this ObjectQuery<TObject> query, CancellationToken token = default)
+            where TObject : BaseInfo
+        {
+            var result = await query.GetEnumerableTypedResultAsync(cancellationToken: token);
+
+            return result?.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the first item of the <paramref name="query"/> as a <see cref="BaseInfo" /> and <see cref="null" /> if no items were returned.
+        /// </summary>
+        /// <param name="query">The current ObjectQuery</param>
+        /// <param name="token">Optional cancellation token</param>
+        /// <returns></returns>
+        public static async Task<BaseInfo?> FirstOrDefaultAsync(this ObjectQuery query, CancellationToken token = default)
+        {
+            var result = await query.GetEnumerableTypedResultAsync(cancellationToken: token);
+
+            return result?.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Prints the provided query's full materialized query text using <see cref="Debug.WriteLine(object?)"/>
+        /// </summary>
+        /// <param name="query">The current ObjectQuery</param>
+        /// <param name="queryName">Optional Name for the query that will denote in the output where this specific query starts and ends.
+        /// If no value is supplied, the filename containing the calling method will be used. If null or an empty string is supplied, name of the generic <see cref="{TObject}" /> will be used.
+        /// </param>
+        /// <example>
+        /// UserInfo.Provider.Get()
+        ///     .WhereEquals("Email", "admin@localhost.local")
+        ///     .Print("User");
+        /// 
+        /// ~~~ BEGIN [User] QUERY ~~~
+        ///
+        /// DECLARE @Email nvarchar(max) = N'admin@localhost.local';
+        ///
+        /// SELECT *
+        /// FROM CMS_User
+        /// WHERE [Email] = @Email
+        ///
+        /// ~~~ END [User] QUERY ~~~
+        /// </example>
+        /// <returns></returns>
+        public static ObjectQuery<TObject> DebugQuery<TObject>(this ObjectQuery<TObject> query, [CallerFilePath] string queryName = "")
+            where TObject : BaseInfo, new()
+        {
+            queryName = string.IsNullOrWhiteSpace(queryName)
+                ? typeof(TObject).Name
+                : queryName;
+
+            Debug.WriteLine(Environment.NewLine);
+            Debug.WriteLine($"~~~ BEGIN [{queryName}] QUERY ~~~");
+            Debug.WriteLine(Environment.NewLine);
+
+            Debug.WriteLine(Environment.NewLine);
+            Debug.WriteLine(query.GetFullQueryText());
+            Debug.WriteLine(Environment.NewLine);
+
+            Debug.WriteLine(Environment.NewLine);
+            Debug.WriteLine($"~~~ END [{queryName}] QUERY ~~~");
+            Debug.WriteLine(Environment.NewLine);
+
+            return query;
+        }
+
+        /// <summary>
+        /// Prints the provided query's full materialized query text using <see cref="Debug.WriteLine(object?)"/>
+        /// </summary>
+        /// <param name="query">The current ObjectQuery</param>
+        /// <param name="queryName">Optional Name for the query that will denote in the output where this specific query starts and ends.
+        /// If no value is supplied, the filename containing the calling method will be used. If null or an empty string is supplied, "BaseInfo" will be used.
+        /// </param>
+        /// <returns></returns>
+        public static ObjectQuery DebugQuery(this ObjectQuery query, [CallerFilePath] string queryName = "")
+        {
+            queryName = string.IsNullOrWhiteSpace(queryName)
+                ? nameof(BaseInfo)
+                : queryName;
+
+            Debug.WriteLine(Environment.NewLine);
+            Debug.WriteLine($"~~~ BEGIN [{queryName}] QUERY ~~~");
+            Debug.WriteLine(Environment.NewLine);
+
+            Debug.WriteLine(Environment.NewLine);
+            Debug.WriteLine(query.GetFullQueryText());
+            Debug.WriteLine(Environment.NewLine);
+
+            Debug.WriteLine(Environment.NewLine);
+            Debug.WriteLine($"~~~ END [{queryName}] QUERY ~~~");
+            Debug.WriteLine(Environment.NewLine);
+
+            return query;
+        }
+
+        /// <summary>
+        /// Prints the provided query's full materialized query text using <see cref="LoggerExtensions.LogDebug(ILogger, string, object[])"/>
+        /// </summary>
+        /// <param name="query">The current ObjectQuery</param>
+        /// <param name="logger">The logger used to output the query</param>
+        /// <param name="queryName">Optional Name for the query that will denote in the output where this specific query starts and ends.
+        /// If no value is supplied, the filename containing the calling method will be used. If null or an empty string is supplied, name of the generic <see cref="{TObject}" /> will be used.
+        /// </param>
+        /// <returns></returns>
+        public static ObjectQuery<TObject> LogQuery<TObject>(this ObjectQuery<TObject> query, ILogger logger, [CallerFilePath] string queryName = "")
+            where TObject : BaseInfo, new()
+        {
+            queryName = string.IsNullOrWhiteSpace(queryName)
+                ? typeof(TObject).Name
+                : queryName;
+
+            logger.LogDebug("Object Query: {queryName} {queryText}", queryName, query.GetFullQueryText());
+
+            return query;
+        }
+
+        /// <summary>
+        /// Prints the provided query's full materialized query text using <see cref="LoggerExtensions.LogDebug(ILogger, string, object[])"/>
+        /// </summary>
+        /// <param name="query">The current ObjectQuery</param>
+        /// <param name="logger">The logger used to output the query</param>
+        /// <param name="queryName">Optional Name for the query that will denote in the output where this specific query starts and ends.
+        /// If no value is supplied, the filename containing the calling method will be used. If null or an empty string is supplied, "BaseInfo" will be used.
+        /// </param>
+        /// <returns></returns>
+        public static ObjectQuery LogQuery(this ObjectQuery query, ILogger logger, [CallerFilePath] string queryName = "")
+        {
+            queryName = string.IsNullOrWhiteSpace(queryName)
+                ? nameof(BaseInfo)
+                : queryName;
+
+            logger.LogDebug("Object Query: {queryName} {queryText}", queryName, query.GetFullQueryText());
+
+            return query;
+        }
+    }
+}
