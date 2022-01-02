8436b0c4188eb490c7da13fd9277123c01228913
Sean Wright <sean@wiredviews.com>
2022-01-01 19:47:29 -0500

    refactor(WhereConditionBaseExtensions): cleanup file

--- src/XperienceCommunity.QueryExtensions/Documents/XperienceCommunityWhereConditionBaseExtensions.cs
+++ src/XperienceCommunity.QueryExtensions/Documents/XperienceCommunityWhereConditionBaseExtensions.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using CMS.DataEngine;
 using CMS.DocumentEngine;
 
-namespace CMS.DataEngine
+// Sourced from https://github.com/wiredviews/xperience-query-extensions/pull/1/files by https://github.com/ChristopherBass
+
+namespace XperienceCommunity.QueryExtensions.Documents
 {
     public static class XperienceCommunityWhereConditionBaseExtensions
     {
@@ -12,68 +13,85 @@ namespace CMS.DataEngine
         /// Filters the data to include only documents on given path(s).
         /// </summary>
         /// <typeparam name="TQuery">Type of the data query</typeparam>
-        /// <param name="baseQuery">The query being filtered upon</param>
+        /// <param name="condition">The query being filtered upon</param>
         /// <param name="paths">List of document paths</param>
         /// <returns>The filtered query</returns>
-        /// <remarks>DocumentQuery.Path() adds parameters to a property "Paths", but if you are building a where condition that needs to 'OR' the path filter, it won't work since DocumentQuery.Path() doesn't add the path filter into the Where logic until query execution.</remarks>
-        public static TQuery WhereInPath<TQuery>(this WhereConditionBase<TQuery> baseQuery, params string[] paths) where TQuery : WhereConditionBase<TQuery>, new()
+        /// <remarks>
+        /// DocumentQuery.Path() adds parameters to a property "Paths", but if you are building a where condition that needs to 'OR' the path filter, 
+        /// it won't work since DocumentQuery.Path() doesn't add the path filter into the Where logic until query execution.
+        /// </remarks>
+        public static TQuery WhereInPath<TQuery>(this WhereConditionBase<TQuery> condition, params string[] paths) where TQuery : WhereConditionBase<TQuery>, new()
         {
-            var whereCondition = new WhereCondition();
             bool combined = paths.Count() > 1;
+            var customWhereCondition = new WhereCondition();
+
             foreach (string current in paths)
             {
-                whereCondition.Or().Where(new IWhereCondition[]
+                customWhereCondition.Or().Where(new IWhereCondition[]
                 {
                     TreePathUtils.GetAliasPathCondition(current, false, combined)
                 });
             }
-            return baseQuery.Where(whereCondition);
+
+            return condition.Where(customWhereCondition);
         }
 
         /// <summary>
         /// Filters the data to include only documents on given path.
         /// </summary>
         /// <typeparam name="TQuery">Type of the data query</typeparam>
-        /// <param name="baseQuery">The query being filtered upon</param>
+        /// <param name="condition">The query being filtered upon</param>
         /// <param name="path">Document path</param>
         /// <param name="type">Path type to define selection scope</param>
         /// <returns>The filtered query</returns>
-        /// <remarks>DocumentQuery.Path() adds parameters to a property "Paths", but if you are building a where condition that needs to 'OR' the path filter, it won't work since DocumentQuery.Path() doesn't add the path filter into the Where logic until query execution.</remarks>
-        public static TQuery WhereInPath<TQuery>(this WhereConditionBase<TQuery> baseQuery, string path, PathTypeEnum type = PathTypeEnum.Explicit) where TQuery : WhereConditionBase<TQuery>, new()
+        /// <remarks>
+        /// DocumentQuery.Path() adds parameters to a property "Paths", but if you are building a where condition that needs to 'OR' the path filter, 
+        /// it won't work since DocumentQuery.Path() doesn't add the path filter into the Where logic until query execution.
+        /// </remarks>
+        public static TQuery WhereInPath<TQuery>(this WhereConditionBase<TQuery> condition, string path, PathTypeEnum type = PathTypeEnum.Explicit) where TQuery : WhereConditionBase<TQuery>, new()
         {
             var paths = new List<string>();
+
             switch (type)
             {
                 case PathTypeEnum.Single:
-                    {
-                        path = SqlHelper.EscapeLikeQueryPatterns(path, true, true, true);
-                        paths.Add(path);
-                        break;
-                    }
+                {
+                    path = SqlHelper.EscapeLikeQueryPatterns(path, true, true, true);
+                    paths.Add(path);
+                    break;
+                }
                 case PathTypeEnum.Children:
-                    {
-                        path = SqlHelper.EscapeLikeQueryPatterns(path, true, true, true);
-                        paths.Add(TreePathUtils.EnsureChildPath(path));
-                        break;
-                    }
+                {
+                    path = SqlHelper.EscapeLikeQueryPatterns(path, true, true, true);
+                    paths.Add(TreePathUtils.EnsureChildPath(path));
+                    break;
+                }
                 case PathTypeEnum.Section:
-                    {
-                        path = SqlHelper.EscapeLikeQueryPatterns(path, true, true, true);
-                        paths.Add(TreePathUtils.EnsureChildPath(path));
-                        paths.Add(TreePathUtils.EnsureSinglePath(path));
-                        break;
-                    }
+                {
+                    path = SqlHelper.EscapeLikeQueryPatterns(path, true, true, true);
+                    paths.Add(TreePathUtils.EnsureChildPath(path));
+                    paths.Add(TreePathUtils.EnsureSinglePath(path));
+                    break;
+                }
+
+                case PathTypeEnum.Explicit:
+                    break;
+                default:
+                    break;
             }
-            var whereCondition = new WhereCondition();
+
             bool combined = paths.Count > 1;
+            var customWhereCondition = new WhereCondition();
+
             foreach (string current in paths)
             {
-                whereCondition.Or().Where(new IWhereCondition[]
+                customWhereCondition.Or().Where(new IWhereCondition[]
                 {
                     TreePathUtils.GetAliasPathCondition(current, false, combined)
                 });
             }
-            return baseQuery.Where(whereCondition);
+
+            return condition.Where(customWhereCondition);
         }
     }
 }
